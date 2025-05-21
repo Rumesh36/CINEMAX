6b8d1466c3f4cfb7d424b607447c5ac96a5d9892
Kranthi <22wj1a6610@gniindia.org>
2025-05-21 12:50:15 +0530

    api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,67 +1,15 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react';
-import MovieSearch from '../utilities/MovieSearch';
+import React from 'react'
+
+
 
 const Home = () => {
 
-  const OMDB_API_KEY = '6e419398'
 
-  const [movieName, setmovieName] = useState('');
-  const [movieData, setmovieData] = useState(null)
+  const OMDB_API_KEY = '6e419398';
 
-
-  const handleChanges = (e) => {
-    
-    setmovieName(e.target.value);
-  };
-
-  const handleSearch = () => {
-    fetch(`http://www.omdbapi.com/?apikey=6e419398&t=${movieName}&plot=full`)
-    .then((response) => response.json())
-    .then((data) =>{
-      setmovieData(data);
-    })
-    .catch((error)=>{
-      console.error(" errrowerfuyg " , error)
-    })
-  };
-
-
-
-
+  
   return (
-    <>
-    <h1 className='text-center'>
-      CineMax Search
-      </h1>
-    <label >Search by Movie Name {"    "}</label>
-    <br />
-    <input type="text"
-    placeholder='enter movie'
-    className='border-2 my-5'
-    onChange={handleChanges}
-    value={movieName}
-     />
-     <br />
-     <button
-     onClick={handleSearch}
-      className='btn'>
-      
-       search </button>
-
-       <div className="mt-4">
-        <h2> You Searched for : {movieName}</h2>
-
-        {movieData && (
-          <div>
-            {movieData.Plot}
-            <img src={movieData.Poster} alt="" />
-          </div>
-        )}
-        
-       </div>
-     
-    </>
+    <div>Home</div>
   )
 }
 
