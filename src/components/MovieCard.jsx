396e900b075c050c6335959ec168d754e18f830b
Rumesh36 <rrumesh54@email.com>
2025-06-25 17:07:26 +0530

    end of the project

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,55 +1,62 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-
 const MovieCard = ({ img, title, director, release, plot }) => {
   const [flipped, setFlipped] = useState(false);
-
-  const handleSwipe = (_, info) => {
-    const swipe = info.offset.x;
-
-    if (Math.abs(swipe) > 100) {
-      setFlipped((prev) => !prev);
-    }
-  };
-
   return (
     <div>
-      <div className="bg-white border-black w-[350px] h-[650px] flex flex-col items-center rounded-xl">
-        
-        {/* Swipeable image area */}
-        <div className="mt-8 w-[300px] h-[450px] cursor-grab [perspective:1000px]">
-          <motion.div
-            className="relative w-full h-full transition-transform duration-75"
-            drag="x"
-            dragConstraints={{ left: 0, right: 0 }}
-            onDragEnd={handleSwipe}
-            animate={{ rotateY: flipped ? 180 : 0 }}
-            style={{ transformStyle: "preserve-3d" }}
+      <div className="bg-white  border-black w-[350px] h-[650px] flex flex-col items-center rounded-xl">
+
+        {/* card flip area */}
+        <div
+          className=" mt-8 rounded-2xl w-[300px] h-[450px] cursor-pointer [perspective:1000px]"
+          onClick={() => setFlipped(!flipped)}
+        >
+          {/* framer */}
+          <motion.div 
+            className="relative w-full h-full transition-transform duration-100"
+            animate={{rotateY:flipped ? 180 : 0 }}
+            style={{transformStyle:"preserve-3d"}}
           >
-            {/* Front Image */}
+            {/* front side of image  */}
+          
             <div className="absolute w-full h-full rounded-2xl backface-hidden">
-              <img
-                className="w-full h-full object-cover rounded-2xl"
-                src={img}
-                alt={title}
-              />
+              <img 
+              className=" w-full h-full object-cover rounded-2xl" 
+              src={img}
+              alt={title} />
+           </div>
+            {/* back side of image -plot */}
+
+            <div
+             className="absolute w-full h-full rounded-2xl  backface-hidden [transform:rotateY(180deg)] overflow-hidden">
+              <img 
+              className="w-full h-full object-cover filter blur-sm brightness-75 scale-105"
+              src={img} 
+              alt={title} />
+
+              <div className="absolute inset-0 flex items-center justify-center p-4">
+              <p className="text-center whitespace-pre-line leading-relaxed px-2 italic tracking-wider font-bold text-white ">
+                {plot}</p>
+                </div>
             </div>
 
-            {/* Back Plot */}
-            <div className="absolute w-full h-full bg-gray-800 text-white font-medium text-md rounded-2xl flex items-center justify-center p-4 backface-hidden [transform:rotateY(180deg)]">
-              <p className="text-center whitespace-pre-line leading-relaxed px-2">{plot}</p>
-            </div>
           </motion.div>
+           </div>
+      
+        <div className="mt-3 flex flex-wrap gap-3 justify-center  w-70">
+          <h1 className="text-xl font-bold bg-gray-900 text-white px-3 py-2 rounded-lg ">
+            {title}
+          </h1>
+          <h1 className="text-xl font-bold bg-gray-200 border-black border-2 px-4 py-2 rounded-lg ">
+            {release}
+          </h1>
+          <h1 className="text-xl font-bold border-2  text-red-600 px-5 py-2 rounded-lg ">
+            {director}
+          </h1>
         </div>
-
-        {/* Static Details */}
-        <div className="mt-3 flex flex-wrap gap-3 justify-center w-70">
-          <h1 className="text-xl font-bold bg-gray-900 text-white px-3 py-2 rounded-lg">{title}</h1>
-          <h1 className="text-xl font-bold bg-gray-200 border-black border-2 px-4 py-2 rounded-lg">{release}</h1>
-          <h1 className="text-xl font-bold border-2 text-red-600 px-5 py-2 rounded-lg">{director}</h1>
-        </div>
-
-        <button className="text-gray-700 font-medium text-lg mt-2">Swipe image to view plot</button>
+        <button className="text-gray-700 font-medium text-lg mt-2">
+          Click on image for plot....
+        </button>
       </div>
     </div>
   );
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -3,11 +3,19 @@ import MovieCard from '../components/MovieCard'
 import persuite from '../assets/Img/persuit of happiness.webp'
 import fightclub from '../assets/Img/club.webp' 
 import interstellar from '../assets/Img/interstellar.webp'
+import Seven from '../assets/Img/Seven.webp'
+import gifted from '../assets/Img/gifted.webp'
+import inception from '../assets/Img/inception.webp'
+import FordvsFerrari from '../assets/Img/Ford.webp'
 
 const Movies = () => {
   return (
-    <div className='pt-20 bg-gray-300 flex flex-wrap items-center justify-center gap-3 pb-10'>
+    <>    
+    <div className='pt-20 bg-gray-300 '>
+      <h1 className='text-center text-2xl text-red-600 font-bold mb-4 underline'>Admin Picks</h1>
 
+    <div className='flex flex-wrap items-center justify-center gap-3 pb-10'>
+       
       <MovieCard 
       img={interstellar}
       title="Interstellar"
@@ -35,7 +43,44 @@ const Movies = () => {
       form an underground fight club that evolves into something much more sinister. As the club spreads, he struggles with his identity and descends into a dangerous spiral of violence, rebellion, and psychological unraveling."
       />
 
+       <MovieCard 
+      img={Seven}
+      title="Seven"
+      director="David Fincher"
+      release=" 1999 "
+      plot={`Two detectives, a rookie and a veteran, hunt a serial killer who uses the seven deadly sins as his motives. As the investigation progresses, they are drawn deeper into a dark, twisted series of crimes that challenge their morals and sanity — leading to a harrowing, unforgettable climax.`}
+      />
+
+       <MovieCard 
+      img={gifted}
+      title="Gifted"
+      director="Marc Webb"
+      release=" 2017 "
+      plot={`Frank Adler, a single man raising his child prodigy niece Mary, wants her to have a normal life. But when her extraordinary mathematical talents catch attention, a custody battle ensues with his mother — threatening to separate them and disrupt the simple life he tries to give her.`}
+       />
+
+      <MovieCard 
+      img={inception}
+      title="Inception"
+      director="Christopher Nolan"
+      release=" 2017 "
+      plot={`Dom Cobb is a skilled thief, the best in the art of extraction: stealing secrets from within the subconscious during dreams. He's offered a chance to have his past erased — but only if he can achieve the impossible: inception — planting an idea in someone's mind. As layers of dreams unfold, reality begins to blur.`}
+      />
+
+
+      <MovieCard 
+      img={FordvsFerrari}
+      title="Ford v Ferrari"
+      director="ames Mangold"
+      release=" 2019 "
+      plot={`American car designer Carroll Shelby and fearless British driver Ken Miles battle corporate odds, personal demons, and the laws of physics to build a revolutionary race car for Ford. Their mission? Defeat Ferrari at the 1966 Le Mans — a 24-hour race that becomes a legend.`}
+      />
+
+
     </div>
+  </div>
+    </>
+
   )
 }
 
